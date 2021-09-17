7607cb2c9ce565f571027c397c2c26f9916c373f
mosspanic <daw_dash@mail.ru>
2021-09-17 17:02:58 +0500

    toast работает изменение атрибутов хромает

--- app/src/main/java/com/example/navigation/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/navigation/ui/SettingsFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Switch
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 //import com.example.bottomnavigationexample.R
 import com.example.navigation.R
 import androidx.fragment.app.Fragment as Fragment1
+import androidx.fragment.app.Fragment as Fragment2
 
-class SettingsFragment: Fragment1(R.layout.fragment_settings){
+class SettingsFragment: Fragment2(R.layout.fragment_settings){
     init{
         println("######################get SettingsFragment")
 
@@ -24,9 +27,22 @@ class SettingsFragment: Fragment1(R.layout.fragment_settings){
         swit1.setOnCheckedChangeListener { buttonView, isChecked ->
             if(isChecked){
                 println("##########Standard Switch is on")
-                //val fs=sw.findViewById<Fragment>(R.id.fset)
+                val fs=sw.findViewById<View>(R.id.fset)
+                val txt="тёмная тема"
+                val duration= Toast.LENGTH_SHORT
+                val toast=Toast.makeText(activity,txt,duration)
+                toast.show()
+                fs.setBackgroundColor(0xFFFFFF)
+                fs.invalidate()
             }else{
                 println("#############Standard Switch is off")
+                val fs=sw.findViewById<View>(R.id.fset)
+                val txt="светлая тема"
+                val duration= Toast.LENGTH_SHORT
+                val toast=Toast.makeText(activity,txt,duration)
+                toast.show()
+                fs.setBackgroundColor(0x0000FF00)
+                fs.invalidate()
             }
         }
         return sw
